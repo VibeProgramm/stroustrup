adf8e75533211fb57f25860244fdb6c6ded4f55f
VibeProgramm <aleksey16.11.2000@gmail.com>
2018-06-01 17:16:08 +0300

    commit 2

--- Project1/console application c++.cpp
+++ Project1/console application c++.cpp
@@ -1,13 +1,17 @@
 //Автор: Мурзин Алексей
 //Hello world
 
-#include <std_lib_facilities.h>
+#include <iostream>
+#include <vector>
+#include <conio.h>
+#include <string>
+using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 
 	cout << "Hello, World!\n";
-
+	
 	
 	_getch();
 	return 0;
