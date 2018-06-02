29587303f11e40b9decf44ad3b81a8e9f3a23ed6
VibeProgramm <aleksey16.11.2000@gmail.com>
2018-06-02 15:47:36 +0300

    повторяющееся слово

---
+++ Глава 1. Проверка на повторяющее слово/console application c++.cpp
@@ -0,0 +1,26 @@
+﻿//Автор: Мурзин Алексей
+//Проверка на повторяющее слово
+
+#include <std_lib_facilities.h>
+int main()
+{
+	SetConsoleRU();
+	
+
+	cout << "Программа проверяет на повторяющие слова\n";
+	cout << "Введите текст/слова" << endl;
+
+	string repeating_word = "";
+	string current;
+
+	//цикл проходит до тех пор пока мы вводим слова. Ввод отделяется пробелом
+	while (cin >> current /*водим текущее слово*/) 
+	{
+		if (current == repeating_word) // Если вводимое слово равно повторяющему
+			//выводим повторяющее слово
+			cout << "Повторяющее слов " << repeating_word << endl;
+		repeating_word = current; //присваем текущее слово повторяющему
+	}
+	
+	return 0;
+}
