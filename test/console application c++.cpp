b3c9f792c5d41fec89f136af591ac19ad6b5ed74
VibeProgramm <aleksey16.11.2000@gmail.com>
2018-06-02 23:27:41 +0300

    test

--- test/console application c++.cpp
+++ test/console application c++.cpp
@@ -7,39 +7,15 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 	
-	cout << "Введите кол-во строк: ";
-	int length;
-	cin >> length;
-	string *s = new string[length];
-
-	cout << "Введите " << length << " строк\n";
 	
+	int a = 1;
+	cout << "a = " << a << endl;
+	cout << "b = ++a*a++" << endl << endl;
+	int b = ++a*a++;
 
-	for (int i = 0; i < length; i++)
-	{
-		cout << "Строка " << i + 1 << " ";
-		//cin >> s[i];
-		cin.get();
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
 
-		getline(cin, s[i]);
-		
-	}
-	srand(time(NULL));
-
-	for (int i = 0; i < length; i++)
-	{
-
-		s[i] = s[rand() % length];
-	}
-	for (int i = 0; i < length; i++)
-	{
-		cout << "\nСтрока " << i + 1  << " " << s[i] << endl;
-	}
-
-	cout << endl;
-	cout << "Рандомная строка " << s[rand() % length];
-
-	
 	
 	
 	_getch();
