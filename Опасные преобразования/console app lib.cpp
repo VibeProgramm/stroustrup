bcd5a57a8063a343e6df4e9eaf21d9e38578dbdc
VibeProgramm <aleksey16.11.2000@gmail.com>
2018-06-05 13:14:16 +0300

    Ошибка сужающих преобразований

---
+++ Опасные преобразования/console app lib.cpp
@@ -0,0 +1,20 @@
+//Автор: Мурзин Алексей
+//Сужающие преобразование
+
+#include <std_lib_facilities.h>
+
+
+int main()
+{
+	SetConsoleRU();
+
+	double d = 2.1;
+
+	//какойт-то код
+
+	int x{ d };
+
+
+	system("pause");
+	return 0;
+}
