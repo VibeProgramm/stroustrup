9eb87675039d1538f61a58f7a5257d3f0baf1a87
VibeProgramm <aleksey16.11.2000@gmail.com>
2018-06-12 16:17:07 +0300

    clear test

--- test/console application c++.cpp
+++ test/console application c++.cpp
@@ -1,26 +1,13 @@
 //Автор: Мурзин Алексей
-//Тестовый файл
+//Перевод из цельсий в фаренгейты
 
 #include <std_lib_facilities.h>
-const int sum()
-{
-	return 2;
-}
-
-
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 	
-	cout << "Введите температуру в цельсиях\n";
-	double celci; cin >> celci;
-
-	double farenge = (double)9 / 5 * celci + 32; //Ели не указать doule перед 9 то будет выполнятья 
-												//целоцисленое деление, а это здесь нам не нужно
-
-	cout << "Температура в фаренгейтах = " << farenge << endl;
-
+	
 
 	_getch();
 	return 0;
