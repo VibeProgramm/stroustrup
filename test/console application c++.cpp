45d46c85976e256bf1f1a71579992b3486d94df6
VibeProgramm <aleksey16.11.2000@gmail.com>
2018-06-21 00:37:11 +0300

    test 21.06.18

--- test/console application c++.cpp
+++ test/console application c++.cpp
@@ -2,23 +2,14 @@
 //Вектор: функция для добавление элемента в вектор
 
 #include <std_lib_facilities.h>
-void add_el(const int el, vector<int> &v)
-{
-	v.push_back(el);
-	cout << "Добавлен один объект в вектор" << endl;
-}
+
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 	
 	
-	vector<int> v{ 1,2,3 };
-	cout << "Размер вектора: " << v.size() << endl;
-	add_el(4, v);
-	cout << "Размер вектора: " << v.size() << endl;
-	for (auto x : v)
-		cout << x << endl;
+	
 
 
 	_getch();
