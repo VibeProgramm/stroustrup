94c45317c10d44a83952d382c06df110404507e4
VibeProgramm <aleksey16.11.2000@gmail.com>
2018-06-21 02:42:18 +0300

    fist commit

---
+++ Считывание в вектор/console app lib.cpp
@@ -0,0 +1,22 @@
+//Автор: Мурзин Алексей
+//
+
+#include <std_lib_facilities.h>
+
+
+int main()
+{
+	SetConsoleRU();
+
+	vector<double> temps;
+	int i = 0;
+
+	for (double temp; cin >> temp; i++) {
+		temps.push_back(temp);
+		cout << temps[i] << endl;
+	}
+
+
+	system("pause");
+	return 0;
+}
