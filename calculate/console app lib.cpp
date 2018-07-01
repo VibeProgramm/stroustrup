d5bdb5c3417b910a4179fb9274f181992835ac68
VibeProgramm <aleksey16.11.2000@gmail.com>
2018-07-01 17:56:00 +0300

    calculate v 1.0

---
+++ calculate/console app lib.cpp
@@ -0,0 +1,36 @@
+//Автор: Мурзин Алексей
+//
+
+#include <std_lib_facilities.h>
+
+
+
+int main()
+{
+	SetConsoleRU();
+
+	cout << "Введите выражение: ";
+	double num_1; cin >> num_1; //cin num 1
+	char c; cin >> c;
+	double num_2; cin >> num_2; //cin num 2
+
+	double sum(0);
+	switch (c)
+	{
+	case '+': sum = num_1 + num_2; break;
+	case '-': sum = num_1 - num_2; break;
+	case '*': sum = num_1 * num_2; break;
+	case '/': sum = num_1 / num_2; break;
+		
+	}
+
+	cout << "Результат: " << sum << endl;
+	
+
+
+
+
+
+	system("pause");
+	return 0;
+}
