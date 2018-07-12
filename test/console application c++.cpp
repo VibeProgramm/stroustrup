d36d0f53f3e5435606979e5bd865c55ed3e8b708
VibeProgramm <aleksey16.11.2000@gmail.com>
2018-07-12 23:14:17 +0600

    test

--- test/console application c++.cpp
+++ test/console application c++.cpp
@@ -3,13 +3,20 @@
 
 #include <std_lib_facilities.h>
 
+class  Token {
+public:
+	char kind;
+	double value;
+
+};
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 
 
-
+	Token t1{ '+' };
+	cout << "kind: " << t1.kind << endl << "value: " << t1.value << endl;
 
 	_getch();
 	return 0;
